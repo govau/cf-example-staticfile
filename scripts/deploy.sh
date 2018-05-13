99c57dbfca50615afdbe092dfca8c10256a7c49f
Brendan Gordon <brendan.gordon@digital.gov.au>
2018-05-14 09:26:26 +1000

    Simplify

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,32 +3,20 @@
 # Exit immediately if there is an error
 set -e
 
-# Cause a pipeline (for example, curl -s http://sipb.mit.edu/ | grep foo) to produce a failure return code if any command errors not just the last command of the pipeline.
-set -o pipefail
-
 # Print shell input lines as they are read.
 set -v
 
-# login to cloud foundry if env vars are present
-login() {
-  if [[ -z "$CF_ORG" ]]; then
-    echo "CF env vars not found, assuming you are already logged in to cf"
-    return
-  fi
-  cf api $CF_API
-  cf auth "$CF_USERNAME" "$CF_PASSWORD"
-  cf target -o $CF_ORG
-  cf target -s $CF_SPACE
-}
+: "${CF_API?Need to set CF_API}"
+: "${CF_USERNAME?Need to set CF_USERNAME}"
+: "${CF_PASSWORD?Need to set CF_PASSWORD}"
+: "${CF_ORG?Need to set CF_ORG}"
+: "${CF_SPACE?Need to set CF_SPACE}"
 
-# main script function
-#
-main() {
+# login to cloud foundry
+cf api $CF_API
+cf auth "$CF_USERNAME" "$CF_PASSWORD"
+cf target -o $CF_ORG
+cf target -s $CF_SPACE
 
-  login()
-
-  cf zero-downtime-push $CF_APP -f manifest.yml
-
-}
-
-main $@
+#push our code to cloud foundry
+cf zero-downtime-push $CF_APP
